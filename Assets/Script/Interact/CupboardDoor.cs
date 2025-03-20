90a6bb67397718c4b9c9ac507f3842a8de423b36
İbrahim Gümüşdal <ibooo2977@gmail.com>
2025-03-20 20:55:11 +0300

    New Kitchen Counter
    Kitchen counter with sink and door opening mechanic done and applied to the kitchen counter

--- Assets/Script/Interact/CupboardDoor.cs
+++ Assets/Script/Interact/CupboardDoor.cs
@@ -5,12 +5,17 @@ public class CupboardDoor : MonoBehaviour, IInteractable
     private bool isOpen = false;
     private Quaternion closedRotation;
     private Quaternion openRotation;
-    public float rotationSpeed = 2f;
+    public float rotationSpeed = 3f;
+
+    [SerializeField] private bool isRightDoor=true; 
 
     void Start()
     {
         closedRotation = transform.rotation;
-        openRotation = closedRotation * Quaternion.Euler(0, 90, 0);
+        if(isRightDoor)
+        openRotation = closedRotation * Quaternion.Euler(0, 0, 90);
+        else
+        openRotation = closedRotation * Quaternion.Euler(0, 0, -90);
     }
 
     void Update()
@@ -18,12 +23,9 @@ public class CupboardDoor : MonoBehaviour, IInteractable
         OpenCupBoard();
     }
 
-    // IInteractable interface'ini implemente et
     public void interact() // Küçük harfle başlayan metod adı
     {
         isOpen = !isOpen;
-        Debug.Log(isOpen ? "Dolap kapağı açıldı!" : "Dolap kapağı kapandı!");
-        
     }
     void OpenCupBoard(){
         if (isOpen)
