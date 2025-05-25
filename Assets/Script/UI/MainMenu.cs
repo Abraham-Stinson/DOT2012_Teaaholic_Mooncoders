7395773562bf44e8d8d5c40867955cdea5b291e9
İbrahim Gümüşdal <ibooo2977@gmail.com>
2025-05-25 20:48:24 +0300

    Main Menu Updated

--- Assets/Script/UI/MainMenu.cs
+++ Assets/Script/UI/MainMenu.cs
@@ -1,10 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject NewGamePanel;
+    public GameObject SettingsPanel;
+    private PlayerMovementAndInteractionSystem inputActions;
+    
     [Header("---------- Scene Names ----------")]
     public string MainScene;
+
+    private void Start()
+    {
+        NewGamePanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+    }
+    void Awake()
+    {
+        inputActions = new PlayerMovementAndInteractionSystem();
+        inputActions.UI.Back.performed += ctx => ClosePanel();
+    }
+
+    void OnEnable()
+    {
+        inputActions.UI.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.UI.Disable();
+    }
+
+    void ClosePanel()
+    {
+        if (NewGamePanel.activeSelf)
+        {
+            NewGamePanel.SetActive(false);
+        }
+
+        if (SettingsPanel.activeSelf)
+        {
+            SettingsPanel.SetActive(false);
+        }
+    }
 
     public void LoadSceneNewGame()
     {
@@ -18,7 +57,6 @@ public class MainMenu : MonoBehaviour
         PlayerPrefs.Save();
 
         SceneManager.LoadScene(MainScene);
-        Debug.Log("YENİ OYUN BAŞLATILIYOR...");
     }
 
     public void LoadSceneContinue()
@@ -27,7 +65,6 @@ public class MainMenu : MonoBehaviour
         if (PlayerPrefs.HasKey("SavedDay"))
         {
             SceneManager.LoadScene(MainScene);
-            Debug.Log("DEVAM EDİLİYOR...");
         }
         else
         {
